c83cdd39ceceb8eab6b4edd53f90cf6b23f428d8
Florence Kavita <florencekavita@MacBook-Air-de-Florence.local>
2016-10-27 03:54:04 +0200

    first

---
+++ tannersgoods.js
@@ -0,0 +1,10 @@
+        var swiper = new Swiper('.swiper-container', {
+            pagination: '.swiper-pagination',
+            slidesPerView: 4,
+            centeredSlides: true,
+            paginationClickable: true,
+            spaceBetween: 30,
+            grabCursor: true
+        });
+
+
